9184713477f79820cafd1724abfd54337fa51610
Michael Nelsen <mike@hedgren.org>
2022-06-25 17:45:13 -0700

    Version 1.1.0
    - Added config copy routine
    - Removing dc config

--- Plugin.cs
+++ Plugin.cs
@@ -1,17 +1,43 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using System.IO;
 
 namespace PhobosModpack {
-	[BepInPlugin("nrg.valheim.phobosmodpack", "Phobos Modpack", "1.0.0")]
-
-	/**
-	 *
-	 */
+	[BepInPlugin("nrg.valheim.phobosmodpack", "Phobos", "1.1.0")]
 	public class PhobosModpack : BaseUnityPlugin {
-		/**
-		 *
-		 */
+		private ConfigEntry<bool> cfgCopy;
+
 		private void Awake() {
-			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+			cfgCopy = Config.Bind(
+				"General", // config section
+				"Copy Configs", // config key
+				true, // default value
+				"If true, all Phobos configs will be automatically copied into your BepInEx config directory." // config description
+			);
+
+			// Setup paths
+			string dllPath = this.Info.Location;
+			FileInfo fi = new FileInfo(dllPath);
+			string phobosPath = fi.Directory.ToString() + Path.DirectorySeparatorChar + "config";
+			string bepinPath = Paths.ConfigPath;
+
+			// Copy config files
+			if (cfgCopy.Value) {
+				if (Directory.Exists(phobosPath)) {
+					string[] files = Directory.GetFiles(phobosPath);
+					foreach (string s in files) {
+						string fileName = Path.GetFileName(s);
+						string destFile = Path.Combine(bepinPath, fileName);
+						File.Copy(s, destFile, true);
+					}
+
+					Logger.LogInfo("all configs copied");
+				} else {
+					Logger.LogError("phobos config path does not exist");
+				}
+			} else {
+				Logger.LogInfo("config copy is false, skipping");
+			}
 		}
 	}
 }
